ac4001ae1e7d2b9de870a3ced1af801d511a58e9
ayeahgodlove <ayeahgodlove5@gmail.com>
2022-11-11 12:37:34 +0100

    correct updated

--- src/components/AggreateTable.js
+++ src/components/AggreateTable.js
@@ -1,20 +1,25 @@
-import { Table } from "antd";
+import { Card, Col, Table } from "antd";
 import React from "react";
 import { useAppStore } from "../contexts/AppStoreContext";
 import { aggregateTableColumns } from "./AggregateTableColumns";
 
 const AggregateTable = () => {
-    const {aggregate} = useAppStore()
+  const { aggregate } = useAppStore();
 
-    return (
-        <>
-        {
-            aggregate.length > 0 && 
-            <Table size="small" rowKey='id' dataSource={aggregate} columns={aggregateTableColumns} />
-        }
-        </>
-    )
+  return (
+    <Col md={12}>
+      {aggregate.length > 0 && (
+        <Card size="small">
+          <Table
+            size="small"
+            rowKey="id"
+            dataSource={aggregate}
+            columns={aggregateTableColumns}
+          />
+        </Card>
+      )}
+    </Col>
+  );
 };
 
 export default AggregateTable;
-
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,7 +7,7 @@ import {
 } from "../components";
 import "../styles/home-page.style.scss";
 import "../styles/bootcamp-page.scss";
-import { Card, Col, Row, Space, Typography } from "antd";
+import { Card, Col, Row, Typography } from "antd";
 import { ROUTES } from "../routes";
 import { useNavigate } from "react-router-dom";
 import AggregateTable from "../components/AggreateTable";
@@ -24,38 +24,46 @@ const HomePage = () => {
         </Col>
         <Col md={12}>
           <header className="header">
-          <Typography.Title style={{textTransform: 'uppercase', letterSpacing: '-2px', marginBottom: '-.2rem'}}>November 18</Typography.Title>
-          <p>Marks the first united nations</p>
-          <p>World Day for the Prevention of, and Healing from Child Sexual Exploitation, Abuse and Violence</p>
+            <Typography.Title
+              style={{
+                textTransform: "uppercase",
+                letterSpacing: "-2px",
+                marginBottom: "-.2rem",
+              }}
+            >
+              November 18
+            </Typography.Title>
+            <p>Marks the first united nations</p>
+            <p>
+              World Day for the Prevention of, and Healing from Child Sexual
+              Exploitation, Abuse and Violence
+            </p>
             <p className="header__text">
               World day for the prevention and healing of child sexual abuse and
               violence.
             </p>
-              <span>Join us to</span>
-              <h4>Light a Candle</h4>
-              <p>and help bring the tragey of child sexual violence out of the darkness</p>
-              <button
-                className="button button_primary"
-                onClick={() => navigate(ROUTES.JOIN)}
-              >
-                Join now
-              </button>
-              <span className="hashtag">#Nov18worldday</span>
+            <span>Join us to</span>
+            <h4>Light a Candle</h4>
+            <p>
+              and help bring the tragey of child sexual violence out of the
+              darkness
+            </p>
+            <button
+              className="button button_primary"
+              onClick={() => navigate(ROUTES.JOIN)}
+            >
+              Join now
+            </button>
+            <span className="hashtag">#Nov18worldday</span>
 
             {/* Add logo here */}
           </header>
         </Col>
       </Row>
       <Row gutter={[16, 32]} align="middle" justify="center">
-        <Col md={12}>
-          <Card size="small">
-            {/* <AggregateChart /> */}
-            <AggregateTable />
-          </Card>
-        </Col>
-        {/* <Col md={12}>
-          <ProfileTable />
-        </Col> */}
+        <AggregateChart />
+        <AggregateTable />
+        <ProfileTable />
       </Row>
     </AppShell>
   );
